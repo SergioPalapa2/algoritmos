944cf6bdb4bd7934b120f6495d4903a78193d5f3
SergioPalapa2 <sergio.palapa.o@gmail.com>
2023-10-26 23:16:04 -0600

    listas

---
+++ 2_1Arrays.py
@@ -0,0 +1,22 @@
+#Selection sort
+#Seleccion ordenada
+#Los arreglos y listas ordenadas son estructuras de datos basicas.
+#El algoritmo de busqueda binaria(binary search) solo funciona en listas ordenadas.
+
+#Ejemplo:
+#Almacenar una lista TODO en un espacio en memoria
+#Usar un array o una lista
+
+
+###Linked list
+#Una lista enlazada nos ofrece la posibilidad de conocer la direccion o ubicacion en memoria
+#de cada elemento en la lista
+#Cada elemento almacena la siguiente direccion
+#Se optimiza el uso de memoria (inserts)
+#Son buenas cuando se leeran los items uno por uno
+
+
+###Array
+#En un arreglo se sabe la posicion de cada item
+#LA posicion de un elemento es un indice(index)
+
