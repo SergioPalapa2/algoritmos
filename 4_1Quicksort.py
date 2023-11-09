8e1636122d875ef1e552d55574ce8870a91c4e23
SergioPalapa2 <sergio.palapa.o@gmail.com>
2023-11-08 20:22:29 -0600

    quicksort

---
+++ 4_1Quicksort.py
@@ -0,0 +1,15 @@
+#Quicksort
+#Es un algoritmo de ordenamiento mucho mas rapido que el algoritmo de ordenamiento por seleccion.
+#Quicksort se basa en una tecnica llamada Divide&Conquer (D&C)
+#D&C es un algoritmo recursivo que se centra en separar en subproblemas similares al original.
+
+#Nota: Consultar algoritmo euclidiano.
+
+#CODE: Sumar todos los numeros y devolver el total
+def sum(arr):
+    total=0
+    for x in arr:
+        total+= x
+    return total
+
+print(sum(1,2,3,))
